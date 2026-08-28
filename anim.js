9064e436e813fd2eabbc0104ebec83daf7295aac
Siti Khumairoh <149320794+Khumairoh058@users.noreply.github.com>
2026-08-28 17:24:19 +0700

    Comment out lyrics data in anim.js
    Commented out lyrics data in anim.js.

--- anim.js
+++ anim.js
@@ -4,10 +4,10 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "At the time", time: 15 },
-  { text: "The whisper of birds", time: 18 },
-  { text: "Lonely before the sun cried", time: 27 },
-  { text: "Fell from the sky", time: 32 },
+  // { text: "At the time", time: 15 },
+  // { text: "The whisper of birds", time: 18 },
+  // { text: "Lonely before the sun cried", time: 27 },
+  // { text: "Fell from the sky", time: 32 },
 ];
 
 // Animar las letras
